72c0ccd12bd1ca67175f38bbe0fdb82cd56b9c8f
murad-dabbous <moradyousef04@gmail.com>
2023-04-12 09:17:46 +0000

    Modified the app.js file.

--- app.js
+++ app.js
@@ -1,19 +1,38 @@
-let username = prompt('Enter your name');
-let gender = prompt('Enter your gender (male / female) ');
-let age = prompt('Enter your age');
-
-if (age <= 0) {
-    alert('The age cannot be less or equal to zero ')
-}
-let text = 'Do you want to skip the welcoming message?';
-if (confirm(text) == false) {
-    if (gender === 'male') {
-        alert(`Welcome Mr. ${username}`);
+const usersAnswers = () => {
+    let username = prompt('Enter your name');
+    let gender = prompt('Enter your gender (male / female) ');
+    let age = prompt('Enter your age');
+    // Three more questions
+    let isEmployee = prompt('Are you an employee? (Yes/No) ');
+    let jsLikable = prompt('Do you like Javascript? (Yes/No)');
+    let swimming = prompt('Do you like Swimming? (Yes/No)');
+    if (username === '' || gender === '' || age === '' || isEmployee === '' || jsLikable === '') {
+        alert('Invalid');
     }
-    else if (gender === 'female') {
-        alert(`Welcome Ms. ${username}`);
+
+    if (age <= 0) {
+        alert('The age cannot be less or equal to zero ')
+    }
+    let text = 'Do you want to skip the welcoming message?';
+    if (confirm(text) == false) {
+        if (gender === 'male') {
+            alert(`Welcome Mr. ${username}`);
+        }
+        else if (gender === 'female') {
+            alert(`Welcome Ms. ${username}`);
+        }
+        else {
+            alert(`Welcome ${username}`);
+        }
     }
     else {
-        alert(`Welcome ${username}`);
+        alert('Welcome');
     }
-}
+    const answers = [];
+    answers.push(username, gender, age, isEmployee, jsLikable, swimming);
+    for (let i = 0; i < answers.length; i++) {
+        console.log(answers[i]);
+    }
+};
+document.write(usersAnswers());
+
